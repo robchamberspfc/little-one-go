37f12b197204d0bd343ef33199d4a05ca3e775fe
robchamberspfc <robchamberspfc@gmail.com>
2017-09-06 17:33:51 +0100

    try adding second port

--- main.go
+++ main.go
@@ -7,4 +7,7 @@ import (
 func main() {
 	http.Handle("/", http.FileServer(http.Dir("./main")))
 	http.ListenAndServe(":3000", nil)
+
+	http.Handle("/", http.FileServer(http.Dir("./main")))
+	http.ListenAndServe("web1", nil)
 }
